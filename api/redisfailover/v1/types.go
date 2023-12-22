305ce5617f2520ba1d91a145daeb5e2293f7c6a9
Artur Zheludkov <rurkss@gmail.com>
2023-12-22 13:30:27 -0500

    Update api/redisfailover/v1/types.go
    Co-authored-by: Aaron Kuehler <aaron.kuehler@powerhrg.com>

--- api/redisfailover/v1/types.go
+++ api/redisfailover/v1/types.go
@@ -175,6 +175,7 @@ type BootstrapSettings struct {
 	Host           string `json:"host,omitempty"`
 	Port           string `json:"port,omitempty"`
 	AllowSentinels bool   `json:"allowSentinels,omitempty"`
+        // +kubebuilder:default:true
 	Enabled        bool   `json:"enabled,omitempty"`
 }
 
